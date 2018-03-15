b3c011a8bfa9f05e61595a9f23edca02f41c2a6f
Monmeng <843354450@qq.com>
2018-03-15 17:20:00 +0800

    add 03-check

--- 02-web-control/04-progress-bar/index.js
+++ 02-web-control/04-progress-bar/index.js
@@ -1,14 +1,18 @@
 var result=0;
 function start(){  
   var range=document.getElementById('pro');
-  result= setInterval(function(){
-    if(range.value!=100){
-      range.value++;
-    }
-  },100);
-}
+  if(result==0){
+    result= setInterval(function(){
+       if(range.value!=100){
+       range.value++;
+       }
+    },100);
+
+  }
+ }
 function stop(){
   clearInterval(result);
+  result=0;
 }
 function reset(){
   var range=document.getElementById('pro');
--- 03-check/01-check-char/index.js
+++ 03-check/01-check-char/index.js
@@ -1,19 +1,32 @@
 window.onload=function(){
-  document.getElementById('width').focus();
+var width=document.getElementById("width");
+var height=document.getElementById("height");
+var clu=document.getElementById("clu");
+var area=document.getElementById("area");
+var circ=document.getElementById("circ");
+width.onkeypress=function(e) {
+      validateKey(e);      
 }
-function wif(){
+height.onkeypress=function(e) {
+      validateKey(e);       
+}
+function validateKey(e){
+      if(/[abcdf-zABCDF-Z`~!@#$%^&*()=_+\[\]{}|;:'",<>/?\\]/.test(e.key)) e.preventDefault();
+
+}
+width.onblur=function(){
   var width=document.getElementById('width');
   if(width.value==''){
      var a=document.getElementById('a');
       a.innerHTML="宽度不能为空！";
       width.focus();
   }
-  else{
+  else{ 
     if(isNaN(width.value)==true){
-      var a=document.getElementById('a');
-      a.innerHTML='宽度必须是数值！';
+      var a=document.getElementById("a");
+      a.innerHTML="宽度必须是数值";
       width.focus();
-    }else{
+    } else{
       if(width.value<0){
         var a=document.getElementById('a');
         a.innerHTML="宽度必须大于零";
@@ -22,10 +35,10 @@ function wif(){
         var a=document.getElementById('a');
         a.innerHTML='';
       }
-     }
+    }
   }
 }
-function hif(){
+height.onblur=function(){
   var height=document.getElementById('height');
   if(height.value==''){
      var b=document.getElementById('b');
@@ -34,8 +47,8 @@ function hif(){
   }
   else{
     if(isNaN(height.value)==true){
-      var b=document.getElementById('b');
-      b.innerHTML='宽度必须是数值！';
+      var b=document.getElementById("b");
+      b.innerHTML="宽度必须是数值";
       height.focus();
     }else{
       if(height.value<0){
@@ -50,8 +63,12 @@ function hif(){
   }
 }
 
-function jisuan(){ 
-  if(a.innerHTML=='' && b.innerHTML==''){
+clu.onclick=function(){
+   var area=document.getElementById('area');
+   var circ=document.getElementById('circ');
+   circ.value="";
+   area.value="";
+   if(a.innerHTML=='' && b.innerHTML==''){
      var width=document.getElementById('width');
      var w=width.value;
      var height=document.getElementById('height');
@@ -62,10 +79,6 @@ function jisuan(){
      var jarea=parseFloat((w*h).toFixed(5));
      circ.value=jcirc;
      area.value=jarea;
-  }else{
-    var circ=document.getElementById("circ");
-    var area=document.getElementById("area");
-    circ.innerHTML="";
-    area.innerHTML="";
   }
+}
 }
